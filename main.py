1365120cb003501719e12a4c48a379d70eb13a00
Marin <marin.petkov@hashicorp.com>
2023-12-11 17:36:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # The script tests if provided variable is pass
 # import sys
 # check = sys.argv[1]
-value1 = asda
+value1 = 1234
 if float(value1):
         print("The provided value can be converted to float")
